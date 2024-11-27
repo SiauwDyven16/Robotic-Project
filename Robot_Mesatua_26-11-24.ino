f5cf487261e088eef1de1a98f0c587b92d35a2c6
Dyven Siauw <siauwdyven@gmail.com>
2024-11-27 12:14:19 +0800

    Update Robot_Mesatua_26-11-24.ino

--- Robot_Mesatua_26-11-24.ino
+++ Robot_Mesatua_26-11-24.ino
@@ -1,41 +1,29 @@
 #include <ESP32Servo.h>
 
-// #include "BluetoothSerial.h"
-// BluetoothSerial ESP_BT;
 
-int PINGANG_KANAN_PIN; Servo PINGGANG_KANAN;
-int PINGGANG_KIRI_PIN; Servo PINGGANG_KIRI;
+Servo PINGGANG_KANAN, 
+PINGGANG_KIRI, 
+BAHU_KANAN, 
+BAHU_KIRI,
+SIKU_KANAN, 
+SIKU_KIRI, 
+TANGAN_KANAN, 
+TANGAN_KIRI;
 
-int PAHA_KANAN_PIN;
-int PAHA_KIRI_PIN;
-
-int LUTUT_KANAN_PIN;
-int LUTUT_KIRI_PIN;
-
-int PERGELANGAN_KAKI_KANAN_PIN;
-int PERGELENGAN_KAKI_KIRI_PIN;
-
-int TELAPAK_KAKI_KANAN_PIN;
-int TELAPAK_KAKI_KIRI_PIN;
-
-int BAHU_KANAN_PIN = 33; Servo BAHU_KANAN;
-int BAHU_KIRI_PIN = 27; Servo BAHU_KIRI;
-
-int SIKU_KANAN_PIN = 32; Servo SIKU_KANAN;
-int SIKU_KIRI_PIN = 26; Servo SIKU_KIRI;
-
-int TANGAN_KANAN_PIN = 34; Servo TANGAN_KANAN;
-int TANGAN_KIRI_PIN = 14; Servo TANGAN_KIRI;
+int BAHU_KANAN_PIN = 33;
+int BAHU_KIRI_PIN = 27;
+int SIKU_KANAN_PIN = 32;
+int SIKU_KIRI_PIN = 26;
+int TANGAN_KANAN_PIN = 34;
+int TANGAN_KIRI_PIN = 14;
 
 void setup() {
   Serial.begin(9600);
-  
   BAHU_KANAN.attach(BAHU_KANAN_PIN);
-  SIKU_KANAN.attach(SIKU_KANAN_PIN);
-  TANGAN_KANAN.attach(TANGAN_KANAN_PIN);
-
   BAHU_KIRI.attach(BAHU_KIRI_PIN);
+  SIKU_KANAN.attach(SIKU_KANAN_PIN);
   SIKU_KIRI.attach(SIKU_KIRI_PIN);
+  TANGAN_KANAN.attach(TANGAN_KANAN_PIN);
   TANGAN_KIRI.attach(TANGAN_KIRI_PIN);
 }
 
@@ -44,22 +32,20 @@ void ACTION_pushup() {
   BAHU_KIRI.write(90);
   delay(1000);
   
-  for (int i = 0; i < 4; i++) {
+ for (int i = 0; i < 4; i++) {
     SIKU_KANAN.write(180);
-    TANGAN_KANAN.write(90);
     SIKU_KIRI.write(0);
+    TANGAN_KANAN.write(90);
     TANGAN_KIRI.write(90);
     delay(500);
 
     SIKU_KANAN.write(90);
-    TANGAN_KANAN.write(180);
     SIKU_KIRI.write(90);
+    TANGAN_KANAN.write(180);
     TANGAN_KIRI.write(90);
     delay(500);
   }
 
-  
-}
 void loop() {
   ACTION_pushup();
 }
